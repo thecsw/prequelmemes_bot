995c7d43950857d79ed2011df8c6f1f3cf26e24f
thecsw <sagindyk.urazayev@protonmail.ch>
2018-04-14 21:15:31 +0400

    Updated the message.
    More formatting.

--- src/message.py
+++ src/message.py
@@ -1,13 +1,16 @@
 
 message = """
-Hello there!
+**Hello there!**
 
 A lot of people do not know where the quotes come from and I am here to help you!
 
-The line "%CITATION%" is from a "%MOVIE%" and starts at %START%, lasting exactly %TIME% and finishes at %END%.
+The line "*%CITATION%*" is from **"%MOVIE%"**.
+
+The line starts at %START% and finishes at %END%.
 
 I hope I was able to help you!
 ___________________________
 ^(I am a small bot. Still need improving. Want to check out? Go to my source code and commit!)
-(Source code)[https://github.com/thecsw/prequelmemes_bot]
+
+[Source code](https://github.com/thecsw/prequelmemes_bot)
 """
