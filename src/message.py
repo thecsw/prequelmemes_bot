35d6ecd5ef7ca2165af5368e965f24e1b76200b9
thecsw <sagindyk.urazayev@protonmail.ch>
2018-04-14 17:56:12 +0400

    Added the main message file.
    This is the file that contains reply to posted submissions.
    Note that:
         %CITATION% is the line that was investigated.
         %MOVIE% is the movie name.
         %START% is the start time of that line from %MOVIE%.
         %END% is the end time of that line from %MOVIE%.
         %TIME% is the duration of the line %CITATION%.

---
+++ src/message.py
@@ -0,0 +1,13 @@
+
+message = """
+Hello there!
+
+A lot of people do not know where the quotes come from and I am here to help you!
+
+The line "%CITATION%" is from a "%MOVIE%" and starts at %START%, lasting exactly %TIME% and finishes at %END%.
+
+I hope I was able to help you!
+___________________________
+^(I am a small bot. Still need improving. Want to check out? Go to my source code and commit!)
+(Source code)[https://github.com/thecsw/prequelmemes_bot]
+"""
