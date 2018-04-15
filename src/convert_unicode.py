9f843a2ec10e40dc238a7c24c86376f2540150ad
thecsw <sagindyk.urazayev@protonmail.ch>
2018-04-15 12:43:08 +0400

    Improved the converting scripts.
    Some subtitles are not properly UTF-8 encoded, so I needed a script that will automatically convert all of them from whatever to utf-8.

--- src/convert_unicode.py
+++ src/convert_unicode.py
@@ -1,13 +1,41 @@
 
-file_name = "Star_Wars_Episode_II_:_Attack_of_the_Clones.srt"
+# The following scripts will just go to the ./subtitles directory and
+# make all the files in it UTF-8 enconding compatible
+# Pretty messy, but hey, It Just Works!
 
-with open(file_name, "r+", encoding="ISO-8859-1") as f:
-    thing = f.read()
-f.close()
+import sys, os
 
-print(thing)
-thing.encode("utf-8")
+folder = "./subtitles/"
 
-with open(file_name, "w+") as f:
-    f.write(thing)
-f.close()
+for (root, dirs, files) in os.walk(folder):   
+
+    for file_name in files:
+        
+        subtitle_name = folder + file_name;
+        print("File name: {}".format(subtitle_name))
+        
+        with open(subtitle_name, "r+", encoding="ISO-8859-1") as f:
+            try:
+                thing = f.read()
+            except Exception as e:
+                print("Failed to read the file.\n\t{}\n".format(e))
+            finally:
+                f.close()
+                print("\tSuccessfully read the file!")
+
+        try:
+            thing.encode("utf-8")
+            print("\tSuccessfully encoded to UTF-8!")
+        except Exception as e:
+            print("Failed at encoding the file to UTF-8.\n\t{}\n".format(e))
+                
+        with open(subtitle_name, "w+") as f:
+            try:
+                f.write(thing)
+            except Exception as e:
+                print("Failed at writing the contents to the file.\n\t{}\n".format(e))
+            finally:
+                f.close()
+                print("\tSuccessfully wrote the file!\n")
+
+    
