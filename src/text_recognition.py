1e54d30eec904a7a55030df9e21c1b4b4a5334d8
thecsw <sagindyk.urazayev@protonmail.ch>
2018-04-17 13:10:12 +0400

    Made a special case for urllib.
    For some reason,debian and arch use urllib differently???

--- src/text_recognition.py
+++ src/text_recognition.py
@@ -6,7 +6,15 @@ from PIL import Image
 
 def text_recognition(post):
     meme_name = "temp"          # We would just call all the temporary images as "temp"
-    urllib.urlretrieve(post.url, filename=meme_name) # Here we are downloading the appropriate image (png, jpg, jpeg, bmp)
+
+    # For some reason different distros behave differently with urllib
+    # Arch linux uses urllib.request.urlretrieve()
+    # Debian and Ubuntu distros use urllib.urlretrieve
+    try:
+        urllib.urlretrieve(post.url, filename=meme_name) # Here we are downloading the appropriate image (png, jpg, jpeg, bmp)
+    except:
+        urllib.request.urlretrieve(post.url, filename=meme_name)
+        
     image = cv2.imread(meme_name)                    # We load up the image using opencv
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Turning the image to grayscale
     gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1] # Making a threshold for the image, text will be more apparent
@@ -15,6 +23,6 @@ def text_recognition(post):
     cv2.imwrite(filename, gray)               # Now, we will save the image
     img = Image.open(filename)                # Open the processed image with pillow
     recognized_text = pytesseract.image_to_string(img).encode('utf-8') # As a means of exceptions, need to read out as UTF-8, so no encoding errors would occur
-    os.remove(filename)         # Deleting all temporary image
+    os.remove(filename)
     os.remove(meme_name)
     return recognized_text      # Returns the recognized text in UTF-8 encodign and with capital and lower letters
