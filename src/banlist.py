c0f8917068ccc3cffecf4441a318a656c62cecfa
Sandy <sagindyk.urazayev@protonmail.ch>
2018-06-16 23:48:01 +0400

    Fixed banlist. Made a list instead of a string.
    It's more understandable this way

--- src/banlist.py
+++ src/banlist.py
@@ -1,1 +1,20 @@
-banlist = "anakinskywalkergeneralgrievousgeneralobiwankenobimasterwinduthesenatepadmewrittenanddirectedbygeorgelucaspalpatinethesenategalacticrepublicdarthvaderthegalaxytalkingaboutareyougoingdarthsidiouswhatareyoudoingjediaboutthisishouldntinthearea"
+
+# This is a list of banned keywords
+# Only words with length > 8 are included
+banlist = ["skywalker",
+           "grievous",
+           "thesenate",
+           "writtenanddirected",
+           "georgelucas",
+           "palpatine",
+           "galacticrepublic",
+           "darthvader",
+           "thegalaxy",
+           "talkingabout",
+           "areyougoing",
+           "darthsidious",
+           "whatareyoudoing",
+           "aboutthis",
+           "shouldnt",
+           "inthearea"
+]
