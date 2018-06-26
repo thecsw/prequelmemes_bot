7030996c9f4052317acaf84780828153167dfdf9
Sandy <sagindyk.urazayev@protonmail.ch>
2018-06-26 12:03:50 +0400

    Refactored code a little bit.

--- src/text_recognition.py
+++ src/text_recognition.py
@@ -5,27 +5,51 @@ import cv2
 from PIL import Image
 
 def extract_image(post):
-    meme_name = "temp"          # We would just call all the temporary images as "temp"
+
+    """
+    This function extracts an image from a reddit post and saves it as 'temp'.
+    For some reason different distros behave differently with urllib
+    Arch linux uses urllib.request.urlretrieve()
+    Debian and Ubuntu distros use urllib.urlretrieve
+    """
+    # We would just call all the temporary images as "temp"
+    meme_name = "temp"          
+    
     try:
-        urllib.urlretrieve(post.url, filename=meme_name) # Here we are downloading the appropriate image (png, jpg, jpeg, bmp)
+        # Here we are downloading the appropriate image (png, jpg, jpeg, bmp)
+        urllib.urlretrieve(post.url, filename=meme_name) 
     except:
         urllib.request.urlretrieve(post.url, filename=meme_name)
+
     return meme_name
         
 def text_recognition(meme_name):
 
-    # For some reason different distros behave differently with urllib
-    # Arch linux uses urllib.request.urlretrieve()
-    # Debian and Ubuntu distros use urllib.urlretrieve
-        
-    image = cv2.imread(meme_name)                    # We load up the image using opencv
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Turning the image to grayscale
-    gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1] # Making a threshold for the image, text will be more apparent
-    gray = cv2.medianBlur(gray, 3) # Adding some blur, useful for really noisy images
-    filename = "{}-ocr.png".format(meme_name) # Making the temporary, ready file for text recognition
-    cv2.imwrite(filename, gray)               # Now, we will save the image
-    img = Image.open(filename)                # Open the processed image with pillow
-    recognized_text = pytesseract.image_to_string(img).encode('utf-8') # As a means of exceptions, need to read out as UTF-8, so no encoding errors would occur
+    # We load up the image using opencv
+    image = cv2.imread(meme_name)                    
+
+    # Turning the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+
+    # Making a threshold for the image, text will be more apparent
+    gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1] 
+
+    # Adding some blur, useful for really noisy images
+    gray = cv2.medianBlur(gray, 3) 
+
+    # Making the temporary, ready file for text recognition
+    filename = "{}-ocr.png".format(meme_name) 
+
+    # Now, we will save the image
+    cv2.imwrite(filename, gray)
+
+    # Open the processed image with pillow
+    img = Image.open(filename)
+
+    # As a means of exceptions, need to read out as UTF-8, so no encoding errors would occur
+    recognized_text = pytesseract.image_to_string(img).encode('utf-8') 
     os.remove(filename)
     os.remove(meme_name)
-    return recognized_text      # Returns the recognized text in UTF-8 encodign and with capital and lower letters
+
+    # Returns the recognized text in UTF-8 encodign and with capital and lower letters
+    return recognized_text
