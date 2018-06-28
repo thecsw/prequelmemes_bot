e2c7ed8d3282067e2d154e2e380b1d421e609f17
Sandy <sagindyk.urazayev@protonmail.ch>
2018-06-28 09:19:25 +0400

    Updated the consts in backup.sh script.

--- backup.sh
+++ backup.sh
@@ -3,4 +3,4 @@
 _now=$(date +"%m_%d_%Y")
 _file="/data/prequelmemes_bot/backups/backup_$_now.sql"
 echo "Starting backup to $_file..."
-docker-compose run --rm postgres pg_dump -h postgres -p 5432 -d prequelmemes_db -U prequelmemer -W -f "$_file"
+docker-compose run --rm postgres pg_dump -h HOST_NAME -p PORT -d DATABASE_NAME -U USERNAME -W -f "$_file"
