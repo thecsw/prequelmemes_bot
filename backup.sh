5c9aa5189e2e6633f04132f8d8e1a2f517d4fd9d
Sandy <sagindyk.urazayev@protonmail.ch>
2018-06-29 12:10:00 +0400

    Updated and edited.

--- backup.sh
+++
@@ -1,6 +0,0 @@
-
-#!/bin/bash
-_now=$(date +"%m_%d_%Y")
-_file="/data/prequelmemes_bot/backups/backup_$_now.sql"
-echo "Starting backup to $_file..."
-docker-compose run --rm postgres pg_dump -h HOST_NAME -p PORT -d DATABASE_NAME -U USERNAME -W -f "$_file"
